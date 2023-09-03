af5b4db921360c6b0de363ba67efd18968b4ceb8
Gregory Karastergios <gregory@gregnk.com>
2023-09-03 10:22:06 -0400

    Made comment headings consistent with the markdown files

--- discord-attachments-downloader.py
+++ discord-attachments-downloader.py
@@ -6,26 +6,26 @@ By Gregory Karastergios
 
 
 Description
-==========================
+------------------------
 A script which automatically downloads the attachments 
 of Discord messages from a Discord data package
 
 Usage
-==========================
+------------------------
 py ./discord-attachments-downloader.py [index]
 
 index is an optional parameter which specifies which part of the channel 
 list the downloader should start at.
 
 How to use
-==========================
+------------------------
 1. Place this script at the root of the data package folder
 2. Run the script
 
 The files will be downloaded to /attachments
 
 Copyright
-==========================
+------------------------
 (c) 2023 Gregory Karastergios
 
 Permission to use, copy, modify, and/or distribute this software for any
