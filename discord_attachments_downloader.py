5884ed801e07086e57516cb8a5df6e33fd20169c
Gregory Karastergios <gregory@gregnk.com>
2023-11-16 12:33:21 -0500

    v1.4.1

--- discord_attachments_downloader.py
+++ discord_attachments_downloader.py
@@ -1,5 +1,5 @@
 LICENSE_TEXT = '''
-discord-attachments-downloader v1.4.0 (2023-11-14)
+discord-attachments-downloader v1.4.1 (2023-11-16)
 https://github.com/gregnk/discord-attachments-downloader
 
 (c) 2023 Gregory Karastergios
